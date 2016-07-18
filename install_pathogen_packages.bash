6ca822428507ec0d0e14a8325b8728fe3b3dae33
Wei-En <weienwong.93@gmail.com>
2016-07-17 20:28:24 -0700

    Added bash script to clone vim packages

---
+++ install_pathogen_packages.bash
@@ -0,0 +1,18 @@
+cd ~/.vim/bundle
+git clone git://github.com/tpope/vim-commentary.git
+git clone https://github.com/ctrlpvim/ctrlp.vim.git
+git clone https://github.com/mattn/emmet-vim.git
+git clone https://github.com/scrooloose/nerdtree.git
+# Rails vim plugin
+git clone git://github.com/tpope/vim-rails.git 
+git clone git://github.com/tpope/vim-bundler.git
+git clone https://github.com/vim-airline/vim-airline
+# vim snipMate
+git clone https://github.com/tomtom/tlib_vim.git
+git clone https://github.com/MarcWeber/vim-addon-mw-utils.git
+git clone https://github.com/garbas/vim-snipmate.git
+# Extras snippets
+git clone https://github.com/honza/vim-snippets.git
+git clone git://github.com/tpope/vim-fugitive.git
+git clone git://github.com/tpope/vim-sensible.git
+git clone git://github.com/tpope/vim-surround.git
